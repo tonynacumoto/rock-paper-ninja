5660d9bef0b2fbe1490429b67c46085f443f481d
Victor <vick2592@yahoo.com>
2024-05-26 15:10:25 -0400

    IPFS page created

---
+++ packages/nextjs/app/ipfs/page.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Debug Contracts",
+  description: "Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way",
+});
+
+const ipfs: NextPage = () => {
+  return (
+    <>
+      <div className="text-center mt-8 bg-secondary p-10">
+        <h1 className="text-4xl my-0">IPFS Integration</h1>
+        <p className="text-neutral">
+          Store and retrieve data from IPFS filecoin.
+          <br /> Check{" "}
+          <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem] px-1">
+            packages / nextjs / app / ipfs / page.tsx
+          </code>{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ipfs;
