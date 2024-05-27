d4ad00bbfb03d32ff31d29004c939c177d805559
Victor <vick2592@yahoo.com>
2024-05-26 21:40:23 -0400

    lighthouse integration working

--- packages/nextjs/app/ipfs/page.tsx
+++ packages/nextjs/app/ipfs/page.tsx
@@ -6,7 +6,36 @@ export const metadata = getMetadata({
   description: "Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way",
 });
 
+import lighthouse from '@lighthouse-web3/sdk'
+
+const text = `{
+  "id": "gameId",
+  "winner": "player1Address",
+  "loser": "player2Address",
+  "winnerHands": [1,1,1,2,1,3],
+  "loserHands": [3,1,1,3, 1, 2],
+  "date": "timestamp"
+}`;
+const apiKey = process.env.NEXT_PUBLIC_LIGHTHOUSE;
+const name = "shikamaru" //Optional
+
+const response = await lighthouse.uploadText(text, apiKey, name)
+const cid = response.data.Hash
+const fileInfo = await lighthouse.getFileInfo(cid)
+console.log("This is the fileInfo", fileInfo);
+console.log("This is the cid", cid);
+console.log("This the the lighthouse response", response);
+lighthouse.uploadText(text, apiKey, name).then(response => {
+  const cid = response.data.Hash;
+  fetch(`https://gateway.lighthouse.storage/ipfs/${cid}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log("This is the json fileInfo", data);
+    });
+});
+
 const ipfs: NextPage = () => {
+  
   return (
     <>
       <div className="text-center mt-8 bg-secondary p-10">
@@ -19,6 +48,7 @@ const ipfs: NextPage = () => {
           </code>{" "}
         </p>
       </div>
+
     </>
   );
 };
