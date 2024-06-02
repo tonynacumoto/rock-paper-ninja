cad64747f205c6ae34e86497cdd51735abfa657d
Sergiy Dybskiy <s@serg.tech>
2024-06-02 00:14:14 -0700

    ens stuff

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,27 +1,7 @@
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
 const externalContracts = {
-  // "31337": {
-  //   ensRegistry: {
-  //     address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e", // ENS Registry contract address
-  //     abi: [
-  //       {
-  //         constant: false,
-  //         inputs: [
-  //           { name: "node", type: "bytes32" },
-  //           { name: "label", type: "bytes32" },
-  //           { name: "owner", type: "address" },
-  //         ],
-  //         name: "setSubnodeOwner",
-  //         outputs: [{ name: "ret", type: "bytes32" }],
-  //         payable: false,
-  //         stateMutability: "nonpayable",
-  //         type: "function",
-  //       },
-  //     ],
-  //   },
-  // },
-  "1": {
+  31337: {
     ensRegistry: {
       address: "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e", // ENS Registry contract address
       abi: [
@@ -42,5 +22,5 @@ const externalContracts = {
     },
   },
 };
-//@ts-expect-error
-export default externalContracts satisfies GenericContractsDeclaration;
+
+export default externalContracts as GenericContractsDeclaration;
